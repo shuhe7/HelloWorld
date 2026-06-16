1829d50ae14abe80c7a35abe80ae200b81bf1ee1
shuhe7 <3316222035@qq.com>
2026-06-16 21:51:02 +0800

    创建main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void sort(int arr[], int size)
+{
+    
+}
+
+int main()
+{
+    int arr[] = {12,4,5,6,7,2,1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sort(arr,size);
+    return 0;
+}
