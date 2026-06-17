2913755cd1e82b9bd88ac2c693c01f21667311ce
Centos8 <shuhe@localhost.localdomain>
2026-06-16 20:35:53 -0700

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 void sort(int arr[], int size)
 {
-    
+    int tmp = 0;
+    for(int i = 0;i < size - 1; i++)
+    {
+        for(int j = 0; j < size - 1 -i;j++)
+	{
+	    tmp = arr[j];
+	    arr[j] = arr[j + 1];
+	    arr[j+1] = tmp;
+	}
+    }
 }
 
 int main()
